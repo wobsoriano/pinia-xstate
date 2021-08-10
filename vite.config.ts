d019f556c942bc8e61469b5e8e98c0ab12a6ef50
Robert Soriano <sorianorobertc@gmail.com>
2021-08-11 00:34:52 +0800

    add library and demo vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+const resolvePath = (str: string) => path.resolve(__dirname, str);
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const devConfig = defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      compilerOptions: {
+        exclude: resolvePath('node_modules/**'),
+      },
+      "include": ["src/**/*.ts", "src/**/*.d.ts", "src/**/*.vue"]
+    })
+  ],
+  build: {
+    outDir: 'dist-demo',
+  }
+  // base: '/pinia-xstate/',
+});
+
+const prodConfig = defineConfig({
+  build: {
+    lib: {
+      entry: resolvePath('lib/index.ts'),
+      name: 'pinia-xstate',
+      fileName: (format) => `pinia-xstate.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue', 'pinia', 'xstate'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          pinia: 'pinia',
+          xstate: 'xstate',
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      compilerOptions: {
+        rootDir: resolvePath("lib"),
+        exclude: resolvePath('node_modules/**'),
+      },
+      "include": ["src/**/*.ts", "src/**/*.d.ts", "src/**/*.vue"]
+    })
+  ],
+});
+
+export default isProd ? prodConfig : devConfig;
