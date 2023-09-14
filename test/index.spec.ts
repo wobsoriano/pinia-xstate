54f71b08347db43517a10d81202060ce69458e68
David Khourshid <davidkpiano@gmail.com>
2023-09-14 15:02:16 -0400

    Update tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,34 +1,34 @@
-import { createPinia, defineStore, setActivePinia } from 'pinia'
-import { beforeEach, describe, expect, test } from 'vitest'
-import { createMachine } from 'xstate'
-import xstate from '../src'
+import { createPinia, defineStore, setActivePinia } from 'pinia';
+import { beforeEach, describe, expect, test } from 'vitest';
+import { createMachine } from 'xstate';
+import xstate from '../src';
 
 const toggleMachine = createMachine({
   id: 'toggle',
   initial: 'inactive',
   states: {
     inactive: {
-      on: { TOGGLE: 'active' }
+      on: { TOGGLE: 'active' },
     },
     active: {
-      on: { TOGGLE: 'inactive' }
-    }
-  }
+      on: { TOGGLE: 'inactive' },
+    },
+  },
 });
 
-const useToggleStore = defineStore(toggleMachine.id, xstate(toggleMachine))
+const useToggleStore = defineStore(toggleMachine.id, xstate(toggleMachine));
 
 describe('xstate', () => {
   beforeEach(() => {
-    setActivePinia(createPinia())
-  })
+    setActivePinia(createPinia());
+  });
 
   test('toggles', () => {
-    const store = useToggleStore()
-    expect(store.state.value).toBe('inactive')
-    store.send('TOGGLE')
-    expect(store.state.value).toBe('active')
-    store.send('TOGGLE')
-    expect(store.state.value).toBe('inactive')
-  })
-})
+    const store = useToggleStore();
+    expect(store.state.value).toBe('inactive');
+    store.send({ type: 'TOGGLE' });
+    expect(store.state.value).toBe('active');
+    store.send({ type: 'TOGGLE' });
+    expect(store.state.value).toBe('inactive');
+  });
+});
